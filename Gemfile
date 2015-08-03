f784e9249b37b487759a0b9080761a9e8da0b386
Yu Hoshino <yuhoshino@dev.local>
2015-08-03 15:12:57 +0900

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+gem 'rails'
+gem 'sqlite3'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
+
+gem 'bootstrap-sass' # この行を追加
---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,6 @@
+//= require jquery
+//= require bootstrap-sprockets // この行を追加
+//= require bootstrap-material-design/dist/js/material // この行を追加
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
